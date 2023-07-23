7eef6cc4d746b3956ba91113484af9914b84f784
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-07-22 17:20:08 -0700

    fix conflict

--- GUI.py
+++ GUI.py
@@ -23,11 +23,7 @@ from repack import pack_folder_to_blarc
 ###############################################
 ###########    GLOBAL SETTINGS      ###########
 ###############################################
-<<<<<<< Updated upstream
-tool_version = "8.0.0"
-=======
 tool_version = "8.1.0"
->>>>>>> Stashed changes
 
 root = customtkinter.CTk()
 root.title(f"Any Aspect Ratio for Tears of the Kingdom {tool_version}")
