e06a56db0711ac81f2e411cc0564c424fd071b95
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-07-25 16:58:17 -0700

    save screen test

--- script.py
+++ script.py
@@ -123,6 +123,7 @@ def patch_blarc(aspect_ratio, HUD_pos, unpacked_folder, expand_shutter):
         patch_blyt('SystemActionGuide_00', 'P_DecoL_02', 'scale_x', s1)
         patch_blyt('SystemActionGuide_00', 'P_DecoL_02', 'shift_x', (675 + 960*s3)*s1)
         patch_blyt('SystemLoadList_00', 'A_Title_00', 'scale_x', s1)
+        patch_blyt('SystemLoadList_00', 'N_SlideList_00', 'scale_x', 1/s1)
         patch_blyt('SystemLoadList_00', 'N_Header_00', 'scale_x', 1/s1)
         patch_blyt('SystemLoadList_00', 'N_SystemBG_00', 'scale_x', 1/s1)
         patch_blyt('SystemLoadList_00', 'P_DecoL_00', 'scale_x', s1)
