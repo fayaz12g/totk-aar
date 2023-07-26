49cd5fbb89b7d48c099ac0046729bb350324f754
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-07-26 16:05:37 -0700

    test

--- patch.py
+++ patch.py
@@ -598,8 +598,9 @@ def create_patch_files(patch_folder, ratio_value, visual_fixes):
 
 @flag print_values
 @flag offset_shift 0x100
-
+@enabled
 {visual_fix}
+@stop
 // Core Game Fixes
 @enabled
 {replacement_value} {ratiotohex.float2hex(ratio_value)}
