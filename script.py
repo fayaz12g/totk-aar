4ed051f593858bb72d7b79cc960dff6552d28973
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-07-26 16:48:01 -0700

    Update script.py

--- script.py
+++ script.py
@@ -158,8 +158,8 @@ def patch_blarc(aspect_ratio, HUD_pos, unpacked_folder, expand_shutter):
         patch_blyt('SystemSaveLoad_00', 'P_DecoR_00', 'shift_x', (737 + 960*s3)*s1)
         patch_blyt('SystemSaveLoad_00', 'N_InOut_00', 'scale_x', 1/s1) # new
         patch_blyt('SystemSaveLoad_00', 'N_SavePos_00', 'scale_x', s1) # new
-        patch_blyt('SystemSaveLoad_00', 'A_DayAndClear_00', 'shift_x', 200) # new
-        patch_blyt('SystemSaveLoad_00', 'T_Place_00', 'shift_x', 133) # new
+        patch_blyt('SystemSaveLoad_00', 'A_DayAndClear_00', 'shift_x', 170) # new
+        patch_blyt('SystemSaveLoad_00', 'T_Place_00', 'shift_x', 70) # new
         
         if HUD_pos == 'corner':
             print("Shifitng elements for corner HUD")
