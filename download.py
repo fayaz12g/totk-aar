c1d1622a60fdbc7669d2f070dea2b94a165fd1f6
cwfields <christopher.fields@me.com>
2023-07-26 21:18:07 -0400

    your mom

--- download.py
+++ download.py
@@ -5,23 +5,28 @@ def download_extract_copy(controller_id, output_folder):
     import requests
     import zipfile
     import shutil
+    import getpass
 
     # URL of the ZIP file
     zip_url = f"https://github.com/fayaz12g/totk-controllers/raw/main/{controller_id}.zip"
 
-    # Download the ZIP file
-    print("Downloading zip file. This may take up to 10 seconds.")
-    response = requests.get(zip_url)
-    print("Zip file downloaded.")
-    zip_file_path = os.path.join(output_folder, f"{controller_id}.zip")
-    with open(zip_file_path, "wb") as file:
-        print("Writing contents to temp folder.")
-        file.write(response.content)
+    username = getpass.getuser()
+    totk_folder = f"C:/Users/{username}/AppData/Roaming/totk-aar/controllers"
+    zip_file_source = os.path.join(totk_folder, f"{controller_id}.zip")
+
+    if not os.path.isfile(zip_file_source):
+        # Download the ZIP file
+        print("Downloading zip file. This may take up to 10 seconds.")
+        response = requests.get(zip_url)
+        print("Zip file downloaded.")
+        with open(zip_file_source, "wb") as file:
+            print("Writing contents to temp folder.")
+            file.write(response.content)
 
     # Extract the ZIP file
     extract_folder = os.path.join(output_folder, "AAR MOD", "temp")
     print(f"Extracting zip to {extract_folder}. This can also take a few seconds.")
-    with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
+    with zipfile.ZipFile(zip_file_source, "r") as zip_ref:
         zip_ref.extractall(extract_folder)
 
     # Copy the extracted file
@@ -39,6 +44,4 @@ def download_extract_copy(controller_id, output_folder):
 
     # Clean up
     print("Cleaning up old files")
-    os.remove(zip_file_path)
     shutil.rmtree(extract_folder)
-
