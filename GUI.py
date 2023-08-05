b756bd55179bb158686b74a79473b643b3ce7f4c
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-08-04 17:18:18 -0700

    fix 16:10 shop stretching

--- GUI.py
+++ GUI.py
@@ -24,7 +24,7 @@ from repack import pack_folder_to_blarc
 ###########    GLOBAL SETTINGS      ###########
 ###############################################
 
-tool_version = "8.4.1"
+tool_version = "8.5.0"
 
 root = customtkinter.CTk()
 root.title(f"Any Aspect Ratio for Tears of the Kingdom {tool_version}")
