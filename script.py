6986f75aa72d5ffd0843cffde472f4619ca50293
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-08-07 13:50:26 -0700

    Update script.py

--- script.py
+++ script.py
@@ -134,7 +134,7 @@ def patch_blarc(aspect_ratio, HUD_pos, unpacked_folder, expand_shutter2):
         patch_blyt('SystemActionGuide_00', 'P_DecoL_02', 'shift_x', (675 + 960*s3)*s1)
         patch_blyt('SystemLoadList_00', 'A_Title_00', 'scale_x', s1)
         patch_blyt('SystemLoadList_00', 'N_All_00', 'scale_x', 1/s1) # new
-        patch_blyt('SystemLoadList_00', 'S_Scissor_00', 'scale_x', s1) # new
+        patch_blyt('SystemLoadList_00', 'Pa_ScrollBar_00', 'scale_x', s1) # new
         patch_blyt('SystemLoadList_00', 'N_Guide_00', 'scale_x', s1) # new     
         patch_blyt('SystemLoadList_00', 'N_Header_00', 'scale_x', 1/s1)
         patch_blyt('SystemLoadList_00', 'N_SystemBG_00', 'scale_x', 1/s1)
@@ -157,10 +157,9 @@ def patch_blarc(aspect_ratio, HUD_pos, unpacked_folder, expand_shutter2):
         patch_blyt('SystemSaveLoad_00', 'P_DecoR_00', 'scale_x', s1)
         patch_blyt('SystemSaveLoad_00', 'P_DecoR_00', 'shift_x', (737 + 960*s3)*s1)
         patch_blyt('SystemSaveLoad_00', 'N_InOut_00', 'scale_x', 1/s1) # new
-        patch_blyt('SystemSaveLoad_00', 'N_PopupBase_00', 'scale_x', ((1/s1)*.75)) # new
+        patch_blyt('SystemSaveLoad_00', 'N_PopupBase_00', 'scale_x', ((1/s1)*.77)) # new
         patch_blyt('SystemSaveLoad_00', 'W_CheckCover_00', 'scale_x', 1/s1) # new
         patch_blyt('SystemSaveLoad_00', 'A_DayAndClear_00', 'shift_x', 150) # new
-        # patch_blyt('SystemSaveLoad_00', 'T_Place_00', 'shift_x', 1) # new
         
         if HUD_pos == 'corner':
             print("Shifitng elements for corner HUD")
