c174149f5b311f25c94e8bbca331e87f72c1867f
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-08-07 14:07:13 -0700

    Update script.py

--- script.py
+++ script.py
@@ -133,9 +133,8 @@ def patch_blarc(aspect_ratio, HUD_pos, unpacked_folder, expand_shutter2):
         patch_blyt('SystemActionGuide_00', 'P_DecoL_02', 'scale_x', s1)
         patch_blyt('SystemActionGuide_00', 'P_DecoL_02', 'shift_x', (675 + 960*s3)*s1)
         patch_blyt('SystemLoadList_00', 'A_Title_00', 'scale_x', s1)
-        patch_blyt('SystemLoadList_00', 'N_All_00', 'scale_x', 1/s1) # new
-        # patch_blyt('SystemLoadList_00', 'Pa_ScrollBar_00', 'scale_x', s1) # new
-        patch_blyt('SystemLoadList_00', 'N_Guide_00', 'scale_x', s1) # new     
+        patch_blyt('SystemLoadList_00', 'N_Slide_00', 'scale_x', 1/s1) # new
+        patch_blyt('SystemLoadList_00', 'C_CaptureUp_00', 'scale_x', s1) # new 
         patch_blyt('SystemLoadList_00', 'N_Header_00', 'scale_x', 1/s1)
         patch_blyt('SystemLoadList_00', 'N_SystemBG_00', 'scale_x', 1/s1)
         patch_blyt('SystemLoadList_00', 'P_DecoL_00', 'scale_x', s1)
@@ -156,9 +155,9 @@ def patch_blarc(aspect_ratio, HUD_pos, unpacked_folder, expand_shutter2):
         patch_blyt('SystemSaveLoad_00', 'P_DecoL_00', 'shift_x', -(675 + 960*s3)*s1)
         patch_blyt('SystemSaveLoad_00', 'P_DecoR_00', 'scale_x', s1)
         patch_blyt('SystemSaveLoad_00', 'P_DecoR_00', 'shift_x', (737 + 960*s3)*s1)
-        patch_blyt('SystemSaveLoad_00', 'N_InOut_00', 'scale_x', 1/s1) # new
-        patch_blyt('SystemSaveLoad_00', 'N_PopupBase_00', 'scale_x', ((1/s1)*.77)) # new
-        patch_blyt('SystemSaveLoad_00', 'W_CheckCover_00', 'scale_x', 1/s1) # new
+        patch_blyt('SystemSaveLoad_00', 'N_PicLoad_00', 'scale_x', 1/s1) # new
+        patch_blyt('SystemSaveLoad_00', 'N_PopupBase_00', 'scale_x', ((1/s1)*.759)) # new
+        # patch_blyt('SystemSaveLoad_00', 'N_PopupWindow_00', 'scale_x', 1/s1) # new
         patch_blyt('SystemSaveLoad_00', 'A_DayAndClear_00', 'shift_x', 150) # new
         
         if HUD_pos == 'corner':
