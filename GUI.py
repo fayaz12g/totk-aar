ed58a8c88c640693c8aa31482df5909f46403e1e
Fayaz12 Gaming <61674751+fayaz12g@users.noreply.github.com>
2023-12-21 17:42:58 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -27,7 +27,7 @@ from repack import pack_folder_to_blarc
 #### Create Window ####
 #######################
 
-tool_version = "9.0.5"
+tool_version = "9.0.6"
 
 root = customtkinter.CTk()
 root.title(f"Fayaz's Settings {tool_version} for TLOZ: Tears of the Kingdom")
